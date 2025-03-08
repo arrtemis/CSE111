ea27339e7c2ebbf78da96d9eea5fa90d9b02e6f7
arrtemis <maxiealexander4864@gmail.com>
2025-03-08 20:22:27 +0600

    add MoneyTracker and Tester4 classes with initial method stubs

---
+++ Course Works/Lab Assignments/Week 3/ung_task1/MoneyTracker.java
@@ -0,0 +1,28 @@
+public class MoneyTracker {
+
+  public void createTracker(String string) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createTracker'");
+  }
+
+  public void income(int i) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'income'");
+  }
+
+  public void expense(int i) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'expense'");
+  }
+
+  public void showHistory() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'showHistory'");
+  }
+
+  public char[] info() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'info'");
+  }
+  
+}
