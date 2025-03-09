fb38a5547c3355cb30531705bcc9101dc38e0dce
arrtemis <maxiealexander4864@gmail.com>
2025-03-09 07:29:21 +0600

    implement MoneyTracker methods for income, expense, and history tracking

--- Course Works/Lab Assignments/Week 3/ung_task1/MoneyTracker.java
+++ Course Works/Lab Assignments/Week 3/ung_task1/MoneyTracker.java
@@ -1,28 +1,40 @@
 public class MoneyTracker {
+  public double balance;
+  public String name;
+  double spent, added;
 
-  public void createTracker(String string) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'createTracker'");
+  public void createTracker(String name) {
+    balance = 1.0;
+    this.name = name;
   }
 
-  public void income(int i) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'income'");
+  public void income(int amount) {
+    added = amount;
+    balance += amount;
+    System.out.println("Balance updated!");
   }
 
-  public void expense(int i) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'expense'");
+  public void expense(int amount) {
+    if(balance - amount == 0){
+      balance -= amount;
+      System.out.println("You're broke!");
+    }else if(balance - amount < 0){
+      System.out.println("Not enough balance.");
+    }else{
+      spent = amount;
+      balance -= amount;
+      System.out.println("Balance updated.");
+    }
   }
 
   public void showHistory() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'showHistory'");
+    System.out.println("Last added: " + added);
+    System.out.println("Last spent: " + spent);
   }
 
-  public char[] info() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'info'");
+  public String info() {
+    String res = "Name: " + name + "\nCurrent Balance: " + balance;
+    return res;
   }
   
 }
