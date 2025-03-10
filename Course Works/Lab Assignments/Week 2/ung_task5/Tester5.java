2bbc8b9964da240f7dfff9f0115249e355b5f887
arrtemis <maxiealexander4864@gmail.com>
2025-03-10 09:06:40 +0600

    refactor formatting in Test1 and Tester5 classes for improved readability

--- Course Works/Lab Assignments/Week 2/ung_task5/Tester5.java
+++ Course Works/Lab Assignments/Week 2/ung_task5/Tester5.java
@@ -1,7 +1,7 @@
-public class Tester5{
-  public static void main (String args[]){
+public class Tester5 {
+  public static void main(String args[]) {
     Test1 t1 = new Test1();
-    t1.methodB(5,-8);
+    t1.methodB(5, -8);
     Test1 t2 = new Test1();
     t2.methodA();
   }
