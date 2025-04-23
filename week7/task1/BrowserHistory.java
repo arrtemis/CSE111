5b674f45d239c5f780e05003672940d155de31ed
arrtemis <yeameenjayed@gmail.com>
2025-04-23 12:39:41 +0600

    task 1 progressed

--- week7/task1/BrowserHistory.java
+++ week7/task1/BrowserHistory.java
@@ -8,16 +8,35 @@ public class BrowserHistory{
   }
 
   public void showHistory(){
-    for (int i = 0; i < websVisited.length; i++) {
-      System.out.println("Visited: " + websVisited[i]);
+    if(count != 0){
+      System.out.println("Browser History: ");
+      for (int i = 0; i < websVisited.length; i++) {
+        System.out.println(websVisited[i]);
+      }
+    }else{
+      System.out.println("No web pages visited yet.");
     }
   }
   public void currentWeb(){
-
+    if(count != 0){
+      System.out.println("Current web page: " + websVisited[count-1]);
+    }else{
+      System.out.println("You have not visited any website yet!");
+    }
+  }
+  public void back(){
+    count--;
   }
 
   public void visitWeb(String web){
-    
+    if(index < websVisited.length){
+      System.out.println("Visited: " + web);
+      websVisited[index++] = web;
+      count++;
+      System.out.println(count);
+    }else{
+      System.out.println("History is full. Cannot visit www.reddit.com web page.");
+    }
   }
 
 }
