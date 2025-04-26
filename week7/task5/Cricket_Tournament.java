73130a6feb8211d85b2ff7f0c66cbcb7592115b9
arrtemis <yeameenjayed@gmail.com>
2025-04-26 09:21:08 +0600

    finished

--- week7/task5/Cricket_Tournament.java
+++ week7/task5/Cricket_Tournament.java
@@ -1,5 +1,21 @@
 package week7.task5;
 
 public class Cricket_Tournament extends Tournament{
+  public int teams;
+  public String type;
+  public Cricket_Tournament(){
+    super();
+    teams = 0;
+    type = "No type";
+  }
 
+  public Cricket_Tournament(String name, int teams, String type) {
+    super(name);
+    this.teams = teams;
+    this.type = type;
+  }
+
+  public String detail(){
+    return "Cricket Tournament Name: " + getName() + "\n" + "Number of Teams: " + teams + "\n" + "Type: " + type;
+  }
 }
--- week7/task5/Tennis_Tournament.java
+++ week7/task5/Tennis_Tournament.java
@@ -1,5 +1,13 @@
 package week7.task5;
 
 public class Tennis_Tournament extends Tournament {
+  public int players;
+  public Tennis_Tournament(String name, int players){
+    super(name);
+    this.players = players;
+  }
 
+  public String detail(){
+    return "Tenis Tournament Name: " + getName() + "\n" + "Number of Players: " + players;
+  }
 }
