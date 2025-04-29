0002ec9c007bd6f2c4fac07655d811aa079a09a3
arrtemis <yeameenjayed@gmail.com>
2025-04-29 18:55:35 +0600

    rough

---
+++ week8/task1/BBAStudent.java
@@ -0,0 +1,13 @@
+package week8.task1;
+
+public class BBAStudent extends Student {
+  public BBAStudent(){
+    super();
+    setDepartment("BBA");
+  }
+
+  public BBAStudent(String name){
+    super();
+    setName(name);
+  }
+}
