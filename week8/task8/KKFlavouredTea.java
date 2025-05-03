dfebdec881766e402a5ada9542080b3857ef798e
arrtemis <maxiealexander4864@gmail.com>
2025-05-03 18:36:46 +0600

    finished

--- week8/task8/KKFlavouredTea.java
+++ week8/task8/KKFlavouredTea.java
@@ -1,13 +1,13 @@
 package week8.task8;
 
 public class KKFlavouredTea extends KKTea {
-  public KKFlavouredTea(String flavour, int price, int bags){
+  public KKFlavouredTea(String flavour, int price, int bags) {
     super(price, bags);
     name = "KK " + flavour + " Tea";
   }
 
-  public static void updateSoldStatusFlavoured(KKFlavouredTea tea){
-    tea.sold++;
-    
+  public static void updateSoldStatusFlavoured(KKFlavouredTea tea) {
+    sold++;
+    tea.status = true;
   }
 }
--- week8/task8/KKTea.java
+++ week8/task8/KKTea.java
@@ -4,24 +4,31 @@ public class KKTea extends Tea {
   public int weight;
   public int teaBags;
   public static int sold;
+  public static int regularTeaSold;
 
-  public KKTea(int price, int bags){
-    super("KK Regular Tea", 250);
+  public KKTea(int price, int bags) {
+    super("KK Regular Tea", price);
     weight = bags * 2;
     teaBags = bags;
   }
 
-  public static void totalSales(){
+  public static void totalSales() {
+    System.out.println("Total Sales: " + sold);
+    System.out.println("KK Regular Tea: " + regularTeaSold);
+    if (sold - regularTeaSold > 0) {
 
+      System.out.println("KK Flavoured Tea: " + (sold - regularTeaSold));
+    }
   }
 
-  public static void updateSoldStatusRegular(KKTea tea){
+  public static void updateSoldStatusRegular(KKTea tea) {
     sold++;
+    regularTeaSold++;
     tea.status = true;
   }
 
   @Override
-  public void productDetail(){
+  public void productDetail() {
     super.productDetail();
     System.out.println("Weight: " + weight + ", Tea Bags: " + teaBags);
   }
