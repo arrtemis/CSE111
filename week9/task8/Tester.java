f3e9eaf203e44554859ebdde1f3c4d74f0d35906
arrtemis <maxiealexander4864@gmail.com>
2025-05-10 13:25:41 +0600

    add debug print statements to Tester.java methods for better traceability

--- week9/task8/Tester.java
+++ week9/task8/Tester.java
@@ -8,25 +8,38 @@ public class Tester {
     Sue var4 = new Sue();
     Blue var5 = new Crew();
     Blue var6 = new Blue();
-    var1.method1();      
+    System.out.println("var1.method1()");
+    var1.method1();
+    System.out.println("var2.method1()");      
     var2.method1();          
     // var3.method1();   compiler error   
-    var4.method1();      
-    var5.method1();      
-    var6.method1();            
-    var1.method3();      
+    System.out.println("var4.method1()");
+    var4.method1();   
+    System.out.println("var5.method1()");   
+    var5.method1(); 
+    System.out.println("var6.method1()");    
+    var6.method1(); 
+    System.out.println("var1.method3()");           
+    var1.method3();   
+    System.out.println("var2.method3()");   
     var2.method3();    
     // var3.method3();      compiler error
-    ((Blue)var1).method1();   
+    System.out.println("((Blue)var1).method1()");
+    ((Blue)var1).method1();
+    System.out.println("((Crew)var1).method2()");  
     ((Crew)var1).method2();  
     // ((Sue)var1).method3();    compiler error
     // ((Blue)var3).method1(); runtime error  
     // ((Crew)var3).method1();  runtime error
-    ((Sue)var3).method3();    
+    System.out.println("((Sue)var3).method3()");
+    ((Sue)var3).method3();
+    System.out.println("((Moo)var2).method2()");    
     ((Moo)var2).method2();   
     // ((Crew)var3).method2();  runtime error
+    System.out.println("((Moo)var5).method2()");
     ((Moo)var5).method2();   
     // ((Moo)var6).method2();   runtime error
+    System.out.println("((Moo)var2).method1()");
     ((Moo)var2).method1();
   }
 }
