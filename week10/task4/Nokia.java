d0ad29b4c9cad85085fd4bc1031b8499965e9d44
arrtemis <yeameenjayed@gmail.com>
2025-05-15 17:44:44 +0600

    finished task 4

---
+++ week10/task4/Nokia.java
@@ -0,0 +1,44 @@
+package task4;
+
+public class Nokia extends Mobile {
+  public double balance;
+
+  public Nokia(String model, boolean status, String imei, double balance){
+    super(model, imei, status);
+    this.balance = balance;
+  }
+
+  public void rechargeSIMCard(double amount) {
+    if (amount > 0) {
+      balance += amount;
+      System.out.println("Recharge successful. New balance: " + balance);
+    } else {
+      System.out.println("Invalid recharge amount.");
+    }
+
+  }
+
+	public String dialCall(String string) {
+    String countryCode = string.substring(0, 3);
+    String countryName = getCountryName(countryCode);
+    if (countryName != null) {
+      if (simCardStatus) {
+        if (balance > 0) {
+          balance -= 1;
+          return "Dialing " + string + " from " + model + " in " + countryName;
+        } else {
+          return "Insufficient balance to make a call.";
+        }
+      } else {
+        return "SIM card is not activated.";
+      }
+    } else {
+      return "Invalid country code.";
+    }
+	}
+
+  @Override
+  public String toString() {
+    return super.toString() + "\nBalance: " + balance;
+  }
+}
